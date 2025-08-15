b85368311928be7ebff7312a343e58ffb831cbe1
HOLZSCHUCH Nicolas <nicolas.holzschuch@inria.fr>
2025-08-15 10:10:58 +0200

    Scripts to build scipy

---
+++ scripts/scipyPackages.sh
@@ -0,0 +1,261 @@
+#! /bin/sh
+
+# boilerplate: code present in each script
+# Changed install prefix so multiple install coexist
+export PREFIX=$PWD
+export XCFRAMEWORKS_DIR=$PREFIX/Python-aux/
+# $PREFIX/Library/bin so that the new python is in the path, 
+# ~/.cargo/bin for rustc
+OLD_PATH=$PATH
+export PATH=$PREFIX/Library/bin:~/.cargo/bin:$OLD_PATH
+export PYTHONPYCACHEPREFIX=$PREFIX/__pycache__
+export OSX_SDKROOT=$(xcrun --sdk macosx --show-sdk-path)
+export DEBUG="-O3 -Wall"
+export CYTHON_OPTIONS="-DCYTHON_PEP489_MULTI_PHASE_INIT=0 -DCYTHON_USE_DICT_VERSIONS=0 -DCYTHON_USE_PYTYPE_LOOKUP=0"
+# Comment this line to re-download all package source from PyPi
+export USE_CACHED_PACKAGES=1
+# DEBUG="-g"
+export OSX_VERSION=11.5 # $(sw_vers -productVersion |awk -F. '{print $1"."$2}')
+# Numpy sets it to 10.9 otherwise. gfortran needs it to 11.5 (for scipy at least)
+export MACOSX_DEPLOYMENT_TARGET=$OSX_VERSION
+# TODO: remove -3.13 from $PREFIX/build directories, use $ARCH in directory names.
+# export ARCH=$(uname -m)
+# Loading different set of frameworks based on the Application:
+APP=$(basename `dirname $PWD`)
+#
+# Set to 1 if you have gfortran for arm64 installed. gfortran support is highly experimental.
+# You might need to edit the script as well.
+USE_FORTRAN=0
+if [ -e "/usr/local/aarch64-apple-darwin20/lib/libgfortran.dylib" ];then
+	USE_FORTRAN=1
+fi
+
+# Function to download source, using curl for speed, pip if jq is not available:
+# For fast downloads, you need the jq command: https://stedolan.github.io/jq/
+# Source: https://github.com/pypa/pip/issues/1884#issuecomment-800483766
+# Can take version as an optional argument: downloadSource pyFFTW 0.12.0
+# If the directory already exists, do not download it unless USE_CACHED_PACKAGES has been set to 0 above.
+downloadSource() 
+{
+   package=$1
+   if [ -d $package-* ] && [ $USE_CACHED_PACKAGES ];
+   then 
+   	   echo using cached version of $package
+   	   return
+   fi
+   rm -rf $package-*
+   if [ $# -eq 1 ]
+   then
+   	   command=.releases\[.info.version]\[\]\|select\(.packagetype==\"sdist\"\)\|.url
+   else
+   	   command=.releases\[\"$2\"\]\[\]\|select\(.packagetype==\"sdist\"\)\|.url
+   fi
+   echo "Downloading " $package
+   if which jq;
+   then
+   	   # jq exists, let's use it:
+   	   url=https://pypi.org/pypi/${package}/json
+   	   address=`curl -L $url | jq -r $command`
+   	   curl -OL $address
+   else 
+   	   # We do not have jq, let's use pip:
+   	   env NPY_BLAS_ORDER="" NPY_LAPACK_ORDER="" MATHLIB="-lm" python3.13 -m pip download --no-deps --no-binary :all: --no-build-isolation $package $package
+   fi
+   if [ -f $package*.tar.gz ];
+   then
+	   tar xvzf $package*.tar.gz
+	   rm $package*.tar.gz
+   fi
+   if [ -f $package*.zip ];
+   then
+	   unzip $package*.zip
+	   rm $package*.zip
+   fi
+}
+
+# End boilerplate
+# scipy
+if [ $USE_FORTRAN == 1 ];
+then
+	pushd packages 
+	downloadSource scipy 
+	pushd scipy-*  
+	# Install scipy *first* because we edit the source files:
+	if [ ! -d scipy_edited ];
+	then
+		cp -r scipy scipy_unedited
+	else
+		mv scipy_unedited scipy
+	fi
+ 	echo "Installing scipy:" 
+ 	env CC=clang CXX=clang++ \
+ 		SCIPY_USE_PYTHRAN=0 \
+ 		CPPFLAGS="-isysroot $OSX_SDKROOT" CFLAGS="-isysroot $OSX_SDKROOT  $CYTHON_OPTIONS $DEBUG" \
+ 		CXXFLAGS="-isysroot $OSX_SDKROOT  $CYTHON_OPTIONS $DEBUG" LDFLAGS="-isysroot $OSX_SDKROOT $DEBUG " \
+		LDFLAGS="-isysroot $OSX_SDKROOT $DEBUG -L$PREFIX/Frameworks_macosx/lib/ -L/Library/Developer/CommandLineTools/SDKs/MacOSX12.0.sdk/usr/lib -L/usr/local/lib -lgfortran" \
+ 		LDSHARED="clang -v -undefined error -dynamiclib -isysroot $OSX_SDKROOT -lz -L$PREFIX -lpython3.13 -L$PREFIX/Frameworks_macosx/lib/ -lc++ $DEBUG" \
+ 		NPY_BLAS_ORDER="openblas" \
+ 		NPY_LAPACK_ORDER="openblas" \
+ 		MATHLIB="-lm" \
+ 		PLATFORM=macosx \
+ 		SETUPTOOLS_USE_DISTUTILS=stdlib \
+ 		python3.13 -m pip install . -v \
+ 		--no-deps --no-build-isolation \
+ 		-Csetup-args=-Duse-pythran=false -Csetup-args=-Dblas=openblas -Csetup-args=-Dlapack=openblas 
+    # could also try installing with meson install --quiet --no-rebuild
+	echo "After install of scipy:" 
+	find $PREFIX/Library/lib/python3.13/site-packages/scipy -type f -name \*.so -print | wc -l 
+	# Edit some Fortran files to avoid function name collisions 
+	# when creating a massive single library:
+	if [ -d scipy_edited ];
+	then
+		mv scipy scipy_unedited
+		mv scipy_edited scipy
+	else
+		# Too many of these, with side effects, trying to keep it down
+		echo "Editing some fortran files:"
+		# 12 duplicate symbols
+		sed -i bak 's/dset_mu/dset_mu_c16/'        scipy/sparse/linalg/_propack/PROPACK/complex16/zlanbpro.F 
+		sed -i bak 's/dcompute_int/dcompute_ic16/' scipy/sparse/linalg/_propack/PROPACK/complex16/zlanbpro.F 
+		sed -i bak 's/dupdate_nu/dupdate_nu_c16/'  scipy/sparse/linalg/_propack/PROPACK/complex16/zlanbpro.F 
+		sed -i bak 's/dupdate_mu/dupdate_mu_c16/'  scipy/sparse/linalg/_propack/PROPACK/complex16/zlanbpro.F 
+		# 8 duplicate symbols
+		sed -i bak 's/DZERO/DZERO_ODR/' scipy/odr/odrpack/d_odr.f
+		# just this one: 7 duplicate symbols
+		sed -i bak 's/szero/szero_d/' scipy/sparse/linalg/_propack/PROPACK/double/dblasext.F 
+		# 6 duplicate symbols
+		sed -i bak 's/scompute_int/scompute_ic8/' scipy/sparse/linalg/_propack/PROPACK/complex8/clanbpro.F
+		# 4 duplicate symbols
+		sed -i bak 's/sset_mu/sset_mu_c8/'       scipy/sparse/linalg/_propack/PROPACK/complex8/clanbpro.F
+		sed -i bak 's/supdate_mu/supdate_mu_c8/' scipy/sparse/linalg/_propack/PROPACK/complex8/clanbpro.F
+		# 3 duplicate symbols
+		sed -i bak 's/supdate_nu/supdate_nu_c8/' scipy/sparse/linalg/_propack/PROPACK/complex8/clanbpro.F	
+		# 2 duplicate symbols:
+		sed -i bak 's/dzero/dzero_s/' scipy/sparse/linalg/_propack/PROPACK/single/sblasext.F
+		# 1 duplicate symbol (note the space)
+		sed -i bak 's/ izero/ izero_d/' scipy/sparse/linalg/_propack/PROPACK/double/dblasext.F  
+		# Annnd 0! (you need to change the symbol everywhere it is used!) 
+		sed -i bak 's/pizero/pizero_d/' scipy/sparse/linalg/_propack/PROPACK/double/dblasext.F 
+		sed -i bak 's/pizero/pizero_d/' scipy/sparse/linalg/_propack/PROPACK/double/dlanbpro.F 
+	fi
+	echo "Compiling scipy:"
+	# Separate build directories for OSX / iOS using meson
+	mkdir -p build_osx  
+	rm -rf build_osx/*
+	env CC=clang CXX=clang++ \
+		CPPFLAGS="-isysroot $OSX_SDKROOT" \
+		CFLAGS="-isysroot $OSX_SDKROOT  $CYTHON_OPTIONS $DEBUG" \
+		CXXFLAGS="-isysroot $OSX_SDKROOT  $CYTHON_OPTIONS $DEBUG" \
+		LDFLAGS="-isysroot $OSX_SDKROOT $DEBUG -L$PREFIX/Frameworks_macosx/lib/ -L/Library/Developer/CommandLineTools/SDKs/MacOSX12.0.sdk/usr/lib -L/usr/local/lib -lgfortran" \
+		LDSHARED="clang -v -undefined error -dynamiclib -isysroot $OSX_SDKROOT -lz -L$PREFIX -lpython3.13 -L$PREFIX/Frameworks_macosx/lib/ -lc++ $DEBUG" meson . build_osx -Duse-pythran=false -Dblas=openblas -Dlapack=openblas 
+	pushd build_osx  
+	ninja 
+	echo scipy libraries for OSX: 
+	find . -name \*.so -print  
+	echo number of scipy libraries for OSX: 
+	find . -name \*.so -print | wc -l 
+	# 110 libraries by the last count (as of 1.16.1):
+	# Make one big library with all of them:
+	mkdir -p extras
+	rm -rf extras/*
+	# remove object files already incorporated into libraries:
+	for directory in scipy/integrate/libmach_lib.a.p \
+		scipy/integrate/libvode_lib.a \
+		scipy/integrate/liblsoda_lib.a.p \
+		scipy/integrate/libdop_lib.a.p \
+		scipy/optimize/librootfind.a.p \
+		scipy/optimize/librectangular_lsap.a.p \
+		scipy/odr/libodrpack.a.p \
+		scipy/libdummy_g77_abi_wrappers.a.p \
+		scipy/lib_fortranobject.a.p \
+		scipy/optimize/_highspy/libhighs.a.p \
+		scipy/special/libcdflib.a.p \
+		scipy/interpolate/libfitpack_lib.a.p \
+		scipy/interpolate/lib__fitpack.a.p \
+		scipy/stats/_levy_stable/lib_levyst.a.p \
+		scipy/sparse/linalg/_eigen/arpack/libarpack_lib.a \
+		scipy/sparse/linalg/_dsolve/libsuperlu_lib.a.p \
+		scipy/sparse/linalg/_propack/liblib__cpropack.a.p \
+		scipy/sparse/linalg/_propack/liblib__zpropack.a.p \
+		scipy/sparse/linalg/_propack/liblib__dpropack.a.p \
+		scipy/sparse/linalg/_propack/liblib__spropack.a.p 
+		do
+			mkdir -p extras/$directory
+			mv $directory/* extras/$directory/
+			mv $directory/.??* extras/$directory/
+		done
+	# Remove extra files (present in multiple dynamic libraries)
+	for file in scipy/special/_test_internal.cpython-313-darwin.so.p/dd_real_wrappers.cpp.o \
+		scipy/special/_test_internal.cpython-313-darwin.so.p/meson-generated__test_internal.c.o\
+		scipy/special/_ufuncs.cpython-313-darwin.so.p/xsf_wrappers.cpp.o \
+		scipy/special/cython_special.cpython-313-darwin.so.p/_cosine.c.o \
+		scipy/stats/_biasedurn.cpython-313-darwin.so.p/libnpyrandom_distributions.c.o \
+		scipy/special/_special_ufuncs.cpython-313-darwin.so.p/_special_ufuncs.cpp.o \
+		scipy/special/_ellip_harm_2.cpython-313-darwin.so.p/sf_error.cc.o \
+		scipy/special/_ufuncs.cpython-313-darwin.so.p/sf_error.cc.o \
+		scipy/special/_ufuncs_cxx.cpython-313-darwin.so.p/sf_error.cc.o \
+		scipy/special/_gufuncs.cpython-313-darwin.so.p/sf_error.cc.o \
+		scipy/special/_special_ufuncs.cpython-313-darwin.so.p/sf_error.cc.o \
+		scipy/special/_gufuncs.cpython-313-darwin.so.p/_gufuncs.cpp.o \
+		scipy/special/cython_special.cpython-313-darwin.so.p/dd_real_wrappers.cpp.o \
+		scipy/optimize/cython_optimize/_zeros.cpython-313-darwin.so.p/meson-generated__zeros.c.o \
+		scipy/sparse/linalg/_propack/_zpropack.cpython-313-darwin.so.p/meson-generated__zpropack-f2pywrappers.f.o \
+		scipy/sparse/linalg/_propack/_spropack.cpython-313-darwin.so.p/meson-generated__spropack-f2pywrappers.f.o \
+		scipy/sparse/linalg/_propack/_cpropack.cpython-313-darwin.so.p/meson-generated__cpropack-f2pywrappers.f.o \
+		scipy/sparse/linalg/_propack/_dpropack.cpython-313-darwin.so.p/meson-generated__dpropack-f2pywrappers.f.o \
+		scipy/special/_ufuncs_cxx.cpython-313-darwin.so.p/meson-generated__ufuncs_cxx.cpp.o \
+		scipy/special/_ufuncs.cpython-313-darwin.so.p/meson-generated__ufuncs.c.o \
+		scipy/special/_ellip_harm_2.cpython-313-darwin.so.p/meson-generated__ellip_harm_2.c.o \
+		scipy/integrate/_dop.cpython-313-darwin.so.p/meson-generated__dop-f2pywrappers.f.o \
+		scipy/integrate/_lsoda.cpython-313-darwin.so.p/meson-generated__lsoda-f2pywrappers.f.o \
+		scipy/integrate/_vode.cpython-313-darwin.so.p/meson-generated__vode-f2pywrappers.f.o
+		do
+			dir=`dirname $file`
+			mkdir -p extras/$dir
+			mv $file extras/$file
+		done
+	# We need to make sure we load our libopenblas and not /usr/local/lib/libopenblas
+	clang -v -undefined error -dynamiclib \
+		-isysroot $OSX_SDKROOT \
+		-lz -lm -lc++ \
+		-lpython3.13 \
+		-L$PREFIX/build/lib.macosx-${OSX_VERSION}-x86_64-3.13 \
+		-L$PREFIX/Frameworks_macosx/lib -lopenblas \
+		-L. \
+		-O3 -Wall  \
+		`find scipy/ -name \*.o`\
+		-Lscipy -l_fortranobject -ldummy_g77_abi_wrappers \
+		-Lscipy/odr -lodrpack \
+		-Lscipy/integrate -lmach_lib -lvode_lib -llsoda_lib -ldop_lib \
+		-Lscipy/optimize -lrootfind -lrectangular_lsap \
+		-Lscipy/optimize/_highspy -lhighs \
+		-Lscipy/special -lcdflib \
+		-Lscipy/interpolate -lfitpack_lib -l__fitpack \
+		-Lsubprojects/qhull_r -llibqhull_r \
+		-Lscipy/stats/_levy_stable -l_levyst \
+		-Lscipy/sparse/linalg/_eigen/arpack -larpack_lib \
+		-Lscipy/sparse/linalg/_dsolve -lsuperlu_lib \
+		-Lscipy/sparse/linalg/_propack -llib__spropack -llib__dpropack -llib__cpropack -llib__zpropack \
+		-L$PREFIX/Library/lib \
+		`find $PREFIX/Library/lib/python3.13/site-packages -name libnpymath.a` \
+		`find $PREFIX/Library/lib/python3.13/site-packages -name libnpyrandom.a` \
+		-L/usr/local/lib -lgfortran \
+		-o scipy.so  
+	popd  
+	cp build_osx/scipy.so $PREFIX/build/lib.macosx-${OSX_VERSION}-x86_64-3.13 
+	install_name_tool -change $PREFIX/Frameworks_macosx/lib/libopenblas.dylib @rpath/openblas.framework/openblas  $PREFIX/build/lib.macosx-${OSX_VERSION}-x86_64-3.13/scipy.so  
+	# restore directories:
+	mv scipy scipy_edited
+	popd
+	popd
+	# seaborn: data position solved with SEABORN_DATA, set in main App. Let's install it by default. 
+	# Need to prevent seaborn from re-installing numpy-1.22 because we have numpy-1.24 already there, and it doesn't realize that 1.24 satisfies numpy>=1.15.
+	# need both --no-deps and --no-build-isolation
+	echo "Installing seaborn" 
+	python3.13 -m pip install seaborn --upgrade --no-deps --no-build-isolation 
+	echo "Done installing seaborn" 
+	# Same with gym:
+	echo "Installing gym" 
+	python3.13 -m pip install gym --upgrade --no-deps --no-build-isolation 
+	echo "Done installing gym" 
+fi
---
+++ scripts/scipyPackages_iOS.sh
@@ -0,0 +1,202 @@
+#! /bin/sh
+
+# Changed install prefix so multiple install coexist
+export PREFIX=$PWD
+export XCFRAMEWORKS_DIR=$PREFIX/Python-aux/
+# $PREFIX/Library/bin so that the new python is in the path, 
+# ~/.cargo/bin for rustc
+export PATH=$PREFIX/Library/bin:~/.cargo/bin:$PATH
+export PYTHONPYCACHEPREFIX=$PREFIX/__pycache__
+export OSX_SDKROOT=$(xcrun --sdk macosx --show-sdk-path)
+export IOS_SDKROOT=$(xcrun --sdk iphoneos --show-sdk-path)
+export DEBUG="-O3 -g -Wall"
+export CYTHON_OPTIONS="-DCYTHON_PEP489_MULTI_PHASE_INIT=0 -DCYTHON_USE_DICT_VERSIONS=0 -DCYTHON_USE_PYTYPE_LOOKUP=0"
+# export DEBUG="-O3 -g -Wall"
+# TODO: remove -3.13 from $PREFIX/build directories, use $ARCH in directory names.
+# export ARCH=$(uname -m)
+# Loading different set of frameworks based on the Application:
+APP=$(basename `dirname $PWD`)
+#
+# Set to 1 if you have gfortran for arm64 installed. gfortran support is highly experimental.
+# You might need to edit the script as well.
+USE_FORTRAN=0
+if [ -e "/usr/local/aarch64-apple-darwin20/lib/libgfortran.dylib" ];then
+	USE_FORTRAN=1
+fi
+
+PYTHON_VER=3.13
+CODESIGNING_FOLDER_PATH=$PREFIX/iOS
+PRODUCT_BUNDLE_IDENTIFIER=Nicolas-Holzschuch
+
+# This function is for creating frameworks for site-packages Python modules (the ones in .../lib/python3.13/site-packages/
+install_site_package () {
+	DIRECTORY=$PREFIX/Library/lib/python3.13/site-packages
+	PACKAGE=$1
+	LIBRARY=$2
+
+	FULL_MODULE_NAME=Python-$(echo $PACKAGE | tr "/" ".")
+
+	# A bundle identifier; not actually used, but required by Xcode framework packaging
+	FRAMEWORK_BUNDLE_ID=$(echo $PRODUCT_BUNDLE_IDENTIFIER.$FULL_MODULE_NAME | tr "_" "-" | sed "s/^\.//")
+	# The name of the framework folder.
+	FRAMEWORK_FOLDER="Frameworks/$FULL_MODULE_NAME.framework"
+	
+	# If the framework folder doesn't exist, create it.
+	if [ ! -d "$CODESIGNING_FOLDER_PATH/$FRAMEWORK_FOLDER" ]; then
+		echo "Creating framework for $PACKAGE"
+		mkdir -p "$CODESIGNING_FOLDER_PATH/$FRAMEWORK_FOLDER"
+		cp "$CODESIGNING_FOLDER_PATH/dylib-Info-template.plist" "$CODESIGNING_FOLDER_PATH/$FRAMEWORK_FOLDER/Info.plist"
+		plutil -replace CFBundleExecutable -string "$FULL_MODULE_NAME" "$CODESIGNING_FOLDER_PATH/$FRAMEWORK_FOLDER/Info.plist"
+		plutil -replace CFBundleIdentifier -string "$FRAMEWORK_BUNDLE_ID" "$CODESIGNING_FOLDER_PATH/$FRAMEWORK_FOLDER/Info.plist"
+		# Mine:
+		plutil -replace CFBundleName -string $FULL_MODULE_NAME "$CODESIGNING_FOLDER_PATH/$FRAMEWORK_FOLDER/Info.plist"
+		plutil -replace DTPlatformName -string "iphoneos" "$CODESIGNING_FOLDER_PATH/$FRAMEWORK_FOLDER/Info.plist"
+		plutil -replace DTSDKName -string "iphoneos" "$CODESIGNING_FOLDER_PATH/$FRAMEWORK_FOLDER/Info.plist"
+		plutil -replace DTPlatformVersion -string "14.0" "$CODESIGNING_FOLDER_PATH/$FRAMEWORK_FOLDER/Info.plist"
+		plutil -replace MinimumOSVersion -string "14.0" "$CODESIGNING_FOLDER_PATH/$FRAMEWORK_FOLDER/Info.plist"
+	fi
+
+	echo "Installing binary for $FRAMEWORK_FOLDER/$FULL_MODULE_NAME"
+	cp "$LIBRARY" "$CODESIGNING_FOLDER_PATH/$FRAMEWORK_FOLDER/$FULL_MODULE_NAME"
+	# change framework id:
+	install_name_tool -id @rpath/$FULL_MODULE_NAME.framework/$FULL_MODULE_NAME "$CODESIGNING_FOLDER_PATH/$FRAMEWORK_FOLDER/$FULL_MODULE_NAME"
+	# Create a placeholder .fwork file where the .so is supposed to be:
+	echo "$FRAMEWORK_FOLDER/$FULL_MODULE_NAME" > $DIRECTORY/$PACKAGE.cpython-313-iphoneos.fwork 
+	# Create a back reference to the .so file location in the framework (relative to PYTHONHOME, which is $APPDIR/Library)
+	# This is currently not used in the code, AFAICS.
+	LOCAL_LIBRARY=$(echo $DIRECTORY | sed "s|$PREFIX/Library/||")
+	echo $LOCAL_LIBRARY/$PACKAGE.cpython-313-iphoneos.so > "$CODESIGNING_FOLDER_PATH/$FRAMEWORK_FOLDER/$FULL_MODULE_NAME.origin"
+}
+
+# end boilerplate
+#
+# scipy-1.16.1
+if [ $USE_FORTRAN == 1 ];
+then
+	pushd packages
+	pushd scipy-* 
+	mv scipy_edited scipy
+	# Separate build directories for OSX / iOS using meson
+	mkdir -p build_ios 
+	rm -rf build_ios/*
+	env CC=clang CXX=clang++ FC=aarch64-apple-darwin20-gfortran meson . build_ios -Duse-pythran=false -Dblas=openblas -Dlapack=openblas --cross-file ../iphone-osx.meson
+	pushd build_ios 
+	# Something between ninja and meson is preventing the creation of dynamic libraries, creates bundles instead:
+	sed -i bak "s/bundle/shared/" build.ninja
+	# meson wants to use "-undefined dynamic_lookup", which is deprecated on iOS. 
+	# We need "-undefined error" to make sure all libraries have been linked.
+	sed -i bak "s/dynamic_lookup/error/g" build.ninja
+	# Correct location for numpy libraries:
+	sed -i bak "s/Library\/lib\/python3.13\/site-packages\/numpy\/_core\/include\/..\/lib/build\/lib.darwin-arm64-3.13\/numpy/g" build.ninja
+	ninja 
+	echo scipy libraries for iOS:
+	find . -name \*.so -print 
+	echo number of scipy libraries for iOS:
+	find . -name \*.so -print | wc -l
+	#
+	# 110 libraries by the last count (as of 1.16.1):
+	# Make one big library with all of them:
+	mkdir -p extras
+	rm -rf extras/*
+	# remove object files already incorporated into static libraries:
+	for directory in scipy/integrate/libmach_lib.a.p \
+		scipy/integrate/libvode_lib.a \
+		scipy/integrate/liblsoda_lib.a.p \
+		scipy/integrate/libdop_lib.a.p \
+		scipy/optimize/librootfind.a.p \
+		scipy/optimize/librectangular_lsap.a.p \
+		scipy/odr/libodrpack.a.p \
+		scipy/libdummy_g77_abi_wrappers.a.p \
+		scipy/lib_fortranobject.a.p \
+		scipy/optimize/_highspy/libhighs.a.p \
+		scipy/special/libcdflib.a.p \
+		scipy/interpolate/libfitpack_lib.a.p \
+		scipy/interpolate/lib__fitpack.a.p \
+		scipy/stats/_levy_stable/lib_levyst.a.p \
+		scipy/sparse/linalg/_eigen/arpack/libarpack_lib.a \
+		scipy/sparse/linalg/_dsolve/libsuperlu_lib.a.p \
+		scipy/sparse/linalg/_propack/liblib__cpropack.a.p \
+		scipy/sparse/linalg/_propack/liblib__zpropack.a.p \
+		scipy/sparse/linalg/_propack/liblib__dpropack.a.p \
+		scipy/sparse/linalg/_propack/liblib__spropack.a.p 
+	do
+		mkdir -p extras/$directory
+		mv $directory/* extras/$directory/
+		mv $directory/.??* extras/$directory/
+	done
+	# Remove extra files (present in multiple dynamic libraries)
+	for file in scipy/special/_test_internal.cpython-313-darwin.so.p/dd_real_wrappers.cpp.o \
+		scipy/special/_test_internal.cpython-313-darwin.so.p/meson-generated__test_internal.c.o\
+		scipy/special/_ufuncs.cpython-313-darwin.so.p/xsf_wrappers.cpp.o \
+		scipy/special/cython_special.cpython-313-darwin.so.p/_cosine.c.o \
+		scipy/stats/_biasedurn.cpython-313-darwin.so.p/libnpyrandom_distributions.c.o \
+		scipy/special/_special_ufuncs.cpython-313-darwin.so.p/_special_ufuncs.cpp.o \
+		scipy/special/_ellip_harm_2.cpython-313-darwin.so.p/sf_error.cc.o \
+		scipy/special/_ufuncs.cpython-313-darwin.so.p/sf_error.cc.o \
+		scipy/special/_ufuncs_cxx.cpython-313-darwin.so.p/sf_error.cc.o \
+		scipy/special/_gufuncs.cpython-313-darwin.so.p/sf_error.cc.o \
+		scipy/special/_special_ufuncs.cpython-313-darwin.so.p/sf_error.cc.o \
+		scipy/special/_gufuncs.cpython-313-darwin.so.p/_gufuncs.cpp.o \
+		scipy/special/cython_special.cpython-313-darwin.so.p/dd_real_wrappers.cpp.o \
+		scipy/optimize/cython_optimize/_zeros.cpython-313-darwin.so.p/meson-generated__zeros.c.o \
+		scipy/sparse/linalg/_propack/_zpropack.cpython-313-darwin.so.p/meson-generated__zpropack-f2pywrappers.f.o \
+		scipy/sparse/linalg/_propack/_spropack.cpython-313-darwin.so.p/meson-generated__spropack-f2pywrappers.f.o \
+		scipy/sparse/linalg/_propack/_cpropack.cpython-313-darwin.so.p/meson-generated__cpropack-f2pywrappers.f.o \
+		scipy/sparse/linalg/_propack/_dpropack.cpython-313-darwin.so.p/meson-generated__dpropack-f2pywrappers.f.o \
+		scipy/special/_ufuncs_cxx.cpython-313-darwin.so.p/meson-generated__ufuncs_cxx.cpp.o \
+		scipy/special/_ufuncs.cpython-313-darwin.so.p/meson-generated__ufuncs.c.o \
+		scipy/special/_ellip_harm_2.cpython-313-darwin.so.p/meson-generated__ellip_harm_2.c.o \
+		scipy/integrate/_dop.cpython-313-darwin.so.p/meson-generated__dop-f2pywrappers.f.o \
+		scipy/integrate/_lsoda.cpython-313-darwin.so.p/meson-generated__lsoda-f2pywrappers.f.o \
+		scipy/integrate/_vode.cpython-313-darwin.so.p/meson-generated__vode-f2pywrappers.f.o
+	do
+		dir=`dirname $file`
+		mkdir -p extras/$dir
+		mv $file extras/$file
+	done
+	# Making a big scipy library to load many modules (85 out of 118):
+	echo "Making a big scipy library:"  >> $PREFIX/make_ios.log 2>&1
+	clang -v -undefined error -dynamiclib \
+		-arch arm64 -miphoneos-version-min=14.0 \
+		-isysroot $IOS_SDKROOT \
+		-lz -lm -lc++ \
+		-F $PREFIX/ios/Frameworks/arm64-iphoneos -framework Python \
+		-L$PREFIX/build/lib.darwin-arm64-3.13 \
+		-L$PREFIX/Frameworks_iphoneos/lib -lgfortran \
+		-F$PREFIX/Frameworks_iphoneos -framework ios_system -framework openblas\
+		-L. \
+		-O3 -Wall  \
+		`find scipy/ -name \*.o`\
+		-Lscipy -l_fortranobject -ldummy_g77_abi_wrappers \
+		-Lscipy/odr -lodrpack \
+		-Lscipy/integrate -lmach_lib -lvode_lib -llsoda_lib -ldop_lib \
+		-Lscipy/optimize -lrootfind -lrectangular_lsap \
+		-Lscipy/optimize/_highspy -lhighs \
+		-Lscipy/special -lcdflib \
+		-Lscipy/interpolate -lfitpack_lib -l__fitpack \
+		-Lsubprojects/qhull_r -llibqhull_r \
+		-Lscipy/stats/_levy_stable -l_levyst \
+		-Lscipy/sparse/linalg/_eigen/arpack -larpack_lib \
+		-Lscipy/sparse/linalg/_dsolve -lsuperlu_lib \
+		-Lscipy/sparse/linalg/_propack -llib__spropack -llib__dpropack -llib__cpropack -llib__zpropack \
+		-L$PREFIX/build/lib.darwin-arm64-3.13/numpy -lnpymath -lnpyrandom \
+		-o scipy.so  
+	popd  
+	install_site_package scipy/scipy build_ios/scipy.so
+	# Now copy that pandas.fwork for all the dynamic libraries.
+	pushd build_ios
+	for library in `find scipy -name \*.so`
+	do
+		directory=`dirname $library`
+		libname=`basename $library .cpython-313-darwin.so`
+		cp $PREFIX/Library/lib/python3.13/site-packages/scipy/scipy.cpython-313-iphoneos.fwork $PREFIX/Library/lib/python3.13/site-packages/$directory/$libname.cpython-313-iphoneos.fwork
+	done
+	# scipy/sparse/_sparsetools/_sparsetools.cpython-313-darwin.so is installed at scipy/sparse/_sparsetools.cpython-313-darwin.so
+	# I don't make the rules
+	cp $PREFIX/Library/lib/python3.13/site-packages/scipy/scipy.cpython-313-iphoneos.fwork $PREFIX/Library/lib/python3.13/site-packages/scipy/sparse/_sparsetools.cpython-313-darwin.fwork
+	# 
+	popd 
+	mv scipy scipy_edited
+	popd
+	popd
+fi
